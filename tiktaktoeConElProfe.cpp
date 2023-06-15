3758cc8d82b5024b4d1f15ff2fa57601ad16fdfa
Jared Osorno <jaredlares5@gmail.com>
2023-06-15 09:06:38 -0600

    aaaaaah_Panico

--- tiktaktoeConElProfe.cpp
+++ tiktaktoeConElProfe.cpp
@@ -19,6 +19,12 @@ const bool NO_ONE = 'N';
 
 int main()
 {
+
+
+
+
+
+    //aaaaaah
     //variables
     char Player = PlayerSymbol(), Computer, Turn = X;
     vector<char> board(NUM_SQUARES, EMPTY);
