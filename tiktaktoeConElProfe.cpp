d66765902946548107f3c1a0931e3514a06e3aae
Jared Osorno <jaredlares5@gmail.com>
2023-06-17 21:28:26 -0600

    Base Game

--- tiktaktoeConElProfe.cpp
+++ tiktaktoeConElProfe.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 //Funciones
+void Saludo();
 void Instruccions();
 char PlayerSymbol();
 char Oponent(char Player);
-char Winner(const vector<char>& board);
+char WinConditions(const vector<char> board);
+void PlayMode(char Player, vector<char>& board);
+void Winner(vector<char> board);
 
 //Constantes
 const int	NUM_SQUARES = 9;
@@ -19,50 +22,76 @@ const bool NO_ONE = false;
 
 int main()
 {
-    char Player = PlayerSymbol(), Computer, Turn = X;
+    char Player, Computer, Turn = X;
     vector<char> board(NUM_SQUARES, EMPTY);
+    // inicio del juego
+    Saludo();
     Player = PlayerSymbol();
     Computer = Oponent(Player);
     cout << "jugaras como: " << Player << "\n La computadora jugara como: " << Computer << "\n";
-
     Instruccions();
+    while (WinConditions(board) == NO_ONE)
+    {
+        //jugara las X
+        if (WinConditions(board) == NO_ONE)
+        {
+            if (Player == X)
+            {
+                PlayMode(Player, board);
+            }
+            else
+            {
+                PlayMode(Computer, board);
+            }
+        }
+        //jugara las O
+        if (WinConditions(board) == NO_ONE)
+        {
+            if (Player == O)
+            {
+                PlayMode(Player, board);
+            }
+            else
+            {
+                PlayMode(Computer, board);
+            }
+        }
+    }
+  
 
-    board[2] = O;
-    board[4] = O;
-    board[6] = O;
-    Winner(board);
-    //while (Winner(board) == NO_ONE)
-    //{
-
-    //}
 }
+
+void Saludo()
+{
+cout << "Bienvenido a este reto mortal humano\n";
+cout <<	"Te crees lo suficiente para enfrentarme?\n";
+}
+
 void Instruccions()
 {
     cout << "---------- Bienvenido a TikTakToe ----------\n";
     cout << "Su batalla sera legendaria\n";
-    cout << "Selecione una posicion\n";
-    cout << " 0 | 1 | 2 \n";
-    cout << " 3 | 4 | 5 \n";
-    cout << " 6 | 7 | 8 \n";
 }
+
 char PlayerSymbol()
 {
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cout << "quieres jugar como las X, y/n\n";
     char answer;
+    bool exit = false;
     do
     {
-        cout << "Quieres empezar como X? (y/n)\n";
         cin >> answer;
-        if (answer == 'y')
+        if (answer=='y')
         {
             return X;
         }
-    } while (answer=='n'||answer=='y');
+    } while (exit);
     return O;
 }
+
 char Oponent(char Player)
 {
-    if (Player == 'X')
+    if (Player == X)
     {
         return O;
     }
@@ -71,58 +100,109 @@ char Oponent(char Player)
         return X;
     }
 }
-char Winner(const vector<char>& board)
+
+char WinConditions(const vector<char> board)
 {
-    int aux = 0;
     for (int i = 0; i <= 8; i++)
     {
-        cout << "\n" << i << "\n";
         if ((i % 3) == 0 && board[i] == X && board[i] == board[i + 1] && board[i] == board[i + 2])
-        {
-            cout << " Ganaron las X\n";
+        {         
             return X;
         }
         else if ((i % 3) == 0 && board[i] == O && board[i] == board[i + 1] && board[i] == board[i + 2])
-        {
-            cout << " Ganaron las O\n";
+        {          
             return O;
         }
         else if (i == 0 && board[i] == X && board[i] == board[4] && board[i] == board[8])
-        {
-            cout << " Ganaron las X\n";
+        {            
             return X;
         }
         else if (i == 0 && board[i] == O && board[i] == board[4] && board[i] == board[8])
-        {
-            cout << " Ganaron las X\n";
+        {        
             return O;
         }
         else if (i < 3 && board[i] == X && board[i] == board[i + 3] && board[i] == board[i + 6])
-        {
-            cout << " Ganaron las X\n";
+        {          
             return X;
         }
         else if (i < 3 && board[i] == O && board[i] == board[i + 3] && board[i] == board[i + 6])
-        {
-            cout << " Ganaron las O\n";
+        {         
             return O;
         }
         else if (i == 2 && board[i] == X && board[i] == board[4] && board[i] == board[6])
-        {
-            cout << " Ganaron las X\n";
+        {           
             return X;
         }
         else if (i == 2 && board[i] == O && board[i] == board[4] && board[i] == board[6])
-        {
-            cout << " Ganaron las X\n";
+        {             
             return O;
         }
-        else if (board[i] == X || board[i] == O)
-        {
-            aux++;
-        }
-        system("pause");
     }
-    cout << " Nadie ha ganado aun \n";
     return NO_ONE;
+}
+
+void PlayMode(char Player, vector<char>& board)
+{
+    int aux;
+    cout << "Jugaran las " << Player;
+    cout << "\nSelecione una posicion\n";
+
+    for (int i = 0; i < board.size() ; i++)
+    {
+        if (i == 2 || i == 5 || i == 8)
+        {
+            if (board[i] == ' ')
+            {
+                cout << " " << i << "\n";
+            }
+            else
+            {
+                cout << " " << board[i] << "\n";
+
+            }
+        }
+        else
+        {
+            if (board[i] == ' ')
+            {
+                cout << " " << i << " |";
+
+            }
+            else
+            {
+                cout << " " << board[i] << " |";
+            }
+        }
+    }
+
+    cin >> aux;
+    if (Player == X)
+    {
+        if (board[aux] != X)
+        {
+            board[aux] = X;
+        }
+    }
+    else if (Player == O)
+    {
+        if (board[aux] != O)
+        {
+            board[aux] = O;
+        }
+    }
+}
+
+void Winner(vector<char> board )
+{
+    if (WinConditions(board) != NO_ONE)
+    {
+        if (WinConditions(board) == X)
+        {
+            cout << " ganaron las X" << "\n";
+        }
+        else
+        {
+            cout << " ganaron las O" << "\n";
+        }
+    }
 }
